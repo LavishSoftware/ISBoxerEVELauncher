2d53a433be7a2f78c79c9c0173b141c18299e2bf
Jizheng <wanjizheng@gmail.com>
2021-09-10 00:54:04 +0100

    v1.0.0.38
    Change Log:
    Modified the WebRequest URL, the default login method should work again now. The browser login approach is only used when the default WebRequest method failed.

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -72,7 +72,7 @@ namespace ISBoxerEVELauncher.Windows
 
             App.Settings.PropertyChanged += Settings_PropertyChanged;
 
-            this.Title += " (v" + VersionString + " - Beta 1)";
+            this.Title += " (v" + VersionString + ")";
 
 
             NotifyIcon = new System.Windows.Forms.NotifyIcon();
